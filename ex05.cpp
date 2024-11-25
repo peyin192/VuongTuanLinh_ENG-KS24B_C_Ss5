10c4e5190ef7708cef73586c7b8abd8e9acb9d92
peyin192 <vuonglinh190206.qv@gmail.com>
2024-11-25 21:38:05 +0700

    upload ss05-5

---
+++ ex05.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(){
+	int num=1,n=1,val;
+	
+
+    for(int num=1;num<=9;num++){
+	
+    for(int n=1;n<=10;n++){
+    	
+    	
+    	
+    	val = num * n;
+    	
+    	
+    	printf("%d x %d = %d\n",num,n,val);
+	}
+}
+	return 0;
+    
+}
+
